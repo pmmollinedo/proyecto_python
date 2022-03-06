d5e42a3a34762737bb620924eb1f6e064e438d99
pmmollinedo <pmollinedo@outlook.es>
2022-03-06 17:36:29 +0100

    Endpoints package deleted

--- pypass/urls/__init__.py
+++ pypass/urls/__init__.py
@@ -4,6 +4,5 @@ app_name = 'pypass'
 
 urlpatterns = [
     path('auth/', include('pypass.urls.auth_urls')),
-    path('logins/', include('pypass.urls.saved_login_urls')),
-    #path('endpoint/', include('pypass.urls.endpoints_urls'))
+    path('logins/', include('pypass.urls.saved_login_urls'))
 ]
