38ed171f97c87ab53b6c9ae7617638cfcc4d8595
pmmollinedo <pmollinedo@outlook.es>
2022-03-13 02:06:49 +0100

    Profile info in session when login

--- pypass/views/auth_views.py
+++ pypass/views/auth_views.py
@@ -1,13 +1,21 @@
+from django.forms import model_to_dict
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views import View
 
 from pypass.forms.auth_form import LoginForm
+from pypass.models.profile import Profile
 
 
 class PyPassLoginView(LoginView):
     form_class = LoginForm
     template_name = "pypass/auth_login.html"
+
+    def get_success_url(self):
+        self.request.session['profile'] = model_to_dict(Profile.objects.get(user=self.request.user.id))
+        return super(PyPassLoginView, self).get_success_url()
+
+    def form_valid(self, form):
+        return super(PyPassLoginView, self).form_valid(form)
 
 
 class PyPassLogoutView(LogoutView):
